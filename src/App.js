0ae00905bec7efe76215f1bdc33c4446d16f0597
mxa5473 <mxa5473@rit.edu>
2019-07-12 15:51:40 -0400

    Complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
 function App() {
   return (
@@ -23,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
